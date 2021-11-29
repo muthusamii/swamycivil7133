ef325d1b5dd9ac816ea955abcc5613714063741e
muthusamii <swamycivil7133@gmail.com>
2021-11-29 22:41:31 +0530

    learning

---
+++ src/test/java/tcs/com/NewOne/Sample.java
@@ -0,0 +1,26 @@
+package tcs.com.NewOne;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Muthuswamy\\eclipse-workspace\\NewOne\\Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+	
+		driver.get("https://www.facebook.com/");
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
